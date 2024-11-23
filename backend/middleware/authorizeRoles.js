7328129c532f92a0a9fd138c69ea1735de98ec80
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-11-23 17:24:40 +0530

    added authorizeroles middle ware

---
+++ backend/middleware/authorizeRoles.js
@@ -0,0 +1,17 @@
+function authorizeRoles(...allowedRoles) {
+    return (req, res, next) => {
+        if (!req.user) return res.status(401).json({
+            msg: "Not authenticated"
+        });
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                msg: "Forbidden: Access denied"
+            });
+        }
+        next();
+    };
+}
+
+module.exports = authorizeRoles;
+
