37c8f1ab697dceec22843451e457656531dec7bc
mohanNaryana <mohankrishnateja2022@gmail.com>
2024-12-05 19:55:34 +0530

    completed redux first part

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,16 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice  = createSlice({
-    name : "auth",
-    initialState : {
-        loading : false
-    },
-    reducers:{
-        setLoading:(state,action) => {
-            state.loading = action.payload
+const authSlice = createSlice({
+    name: "auth",                // Name of the slice
+    initialState: { loading: false },  // Initial state of the slice
+    reducers: {
+        setLoading: (state, action) => {
+            state.loading = action.payload;  // Updates the `loading` state
         }
     }
-})
+});
 
-export const {setLoading} = authSlice.actions
-export default authSlice.reducer
+
+export const { setLoading } = authSlice.actions;
+export default authSlice.reducer;
